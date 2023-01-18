d29f4ec43680698c0a69d5e45f98760619a68597
Jan Horčička <jan.horcicka@yahoo.com>
2023-01-18 18:09:29 +0100

    Pokus o Dropdown a o Scrolling

--- js/main.js
+++ js/main.js
@@ -15,3 +15,26 @@ $(document).ready(function() {
 
 
 });
+
+//scrolling
+$("#sec-1").click(function() {
+    $('html, body').animate({
+        scrollTop:        $("#one").offset().top-66
+    }, 1000);
+ return false;
+});
+
+$("#sec-2").click(function() {
+    $('html, body') .animate({
+        scrollTop:        $("#two").offset().top-112
+    }, 1000);
+ return false;
+});
+
+$("#sec-3").click(function() {
+    $(' html,body') .animate({
+        scrollTop:        $("#three").offset().top-112
+    }, 1000);
+ return false;
+});
+
