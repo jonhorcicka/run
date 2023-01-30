8b272a251d4c6bdc6e14d2fbdecf1138a7190230
Jan Horčička <jan.horcicka@yahoo.com>
2023-01-30 11:39:23 +0100

    oprava srollu a snaha o responzivita webu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,57 @@
 $(document).ready(function() {
 
     $('.sale-main').slick({
-        // infinite: true,
-        slidesToShow: 1,
-        prevArrow: $('.arrows__left'),
-        nextArrow: $('.arrows__right'),
+        dots: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '30px',
+        responsive: [{
+            breakpoint: 767,
+            settings:{
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                adaptiveHeight: false
+            }
+            
+        }
+    ],
+
+    prevArrow: $('.arrows__left'),
+    nextArrow: $('.arrows__right'),
+    // dots: true,
+    // infinite: false,
+    // speed: 300,
+    // slidesToShow: 4,
+    // slidesToScroll: 4,
+    // responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 3,
+    //       infinite: true,
+    //       dots: true
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1
+    //     }
+    //   }
+    //   // You can unslick at a given breakpoint now by adding:
+    //   // settings: "unslick"
+    //   // instead of a settings object
+    // ]
     });
 
     $('.slider-clients').slick({
